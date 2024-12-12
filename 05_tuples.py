db10868030aa19426642027ba1b2d7df62fbe64b
Daniel Quirós Barroso <daniqb99@icloud.com>
2024-12-12 20:11:32 +0100

    TUPLAS

---
+++ 05_tuples.py
@@ -0,0 +1,22 @@
+### TUPLES ###
+
+
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (35, 1.78, "Daniel", "Quirós")
+print(my_tuple)
+print(type(my_tuple))
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[4])
+print(my_tuple[-6])
+
+print(my_tuple.count('Daniel'))
+
+
+
+
+
+
